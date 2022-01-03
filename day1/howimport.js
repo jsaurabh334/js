481f579e1d4a5a8177373f3c913799b91492990a
jsaurabh334 <60782961+jsaurabh334@users.noreply.github.com>
2022-01-03 12:43:14 +0530

    final

---
+++ day1/howimport.js
@@ -0,0 +1,8 @@
+// const {naam,first} = require("./howexport")
+// console.log(naam);
+// first();
+//remove "type":"module" before using  old import method from pkg.json file or it wont work
+
+import  {naam,first}  from "./howexport.js";
+console.log(naam);
+first();
