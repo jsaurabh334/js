ad4ea180b03aba512f5fdd050d7a36c01aa66c30
jsaurabh334 <60782961+jsaurabh334@users.noreply.github.com>
2022-01-03 14:00:04 +0530

    final

---
+++ day4/01loops.js
@@ -0,0 +1,26 @@
+// In programming, loops are used to repeat a block of code.
+// three types of loops
+// for loop
+// while loop 
+// do while loop
+
+
+// program to display text 5 times
+const n = 5;
+
+// looping from i = 1 to 5
+for (let i = 1; i <= n; i++) {
+    console.log(` this text is repeated ${n} times by saurabh`);
+}
+
+
+
+// program to display numbers from 1 to 5
+const p = 5;
+
+// looping from i = 1 to 5
+// in each iteration, i is increased by 1
+for (let i = 1; i <= p; i++) {
+    console.log(i);     // printing the value of i
+}
+
